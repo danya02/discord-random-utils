0fb6e55b6d1b2b7ac102979e68fdd491c9e83564
Danya Generalov <danya@danya02.ru>
2021-06-22 11:43:10 +0300

    Using library mentioned in Discord's community resources.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from flask_discord_interactions import DiscordInteractions
+import os
 
 app = Flask(__name__)
+discord = DiscordInteractions(app)
 
-@app.route('/')
-def index():
-    return 'hello world'
+app.config["DISCORD_CLIENT_ID"] = os.environ["DISCORD_CLIENT_ID"]
+app.config["DISCORD_PUBLIC_KEY"] = os.environ["DISCORD_PUBLIC_KEY"]
+app.config["DISCORD_CLIENT_SECRET"] = os.environ["DISCORD_CLIENT_SECRET"]
+
+@discord.command()
+def ping(ctx):
+    "Respond with a friendly 'pong'!"
+    return "Pong!"
+
+
+discord.set_route("/interactions")
+
+if os.getenv("TESTING_MODE") == 1:
+    discord.update_slash_commands(guild_id=os.environ["TESTING_GUILD"])
+else:
+    discord.update_slash_commands()
+
+
